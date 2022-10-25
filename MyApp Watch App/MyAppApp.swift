de4db5404733682b84915a280947b090e0d36a4f
hacken01 <patricioarancibia@yahoo.cl>
2022-10-25 13:12:46 -0600

    Initial Commit

---
+++ MyApp Watch App/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp Watch App
+//
+//  Created by Hacken on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MyApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
